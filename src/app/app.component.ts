f5410aadaca0866afbec68df6bc4800a3c36b0e2
Mahmoudd1 <mahmouddsoky6@gmail.com>
2023-05-22 10:05:06 +0300

    Features Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teamApp';
+  newMember:String="";
+  errorMessage:String="";
+  members:String []=[];
+  numOfTeams:number|""="";
+  teams:String[][]=[];
+  setNumberOfTeams(value:String)
+  {
+    this.numOfTeams=Number(value);
+  }
+
+  addMember()
+  {
+    if (this.newMember=="")
+    {
+      this.errorMessage="Name can't be empty";
+      return;
+    }
+    this.errorMessage="";
+    this.members.push(this.newMember);
+    console.log(this.members);
+    this.newMember="";
+  }
+  updateNewMember(a:String)
+  {
+    this.newMember=a;
+  
+  }
+  setTeams()
+  {
+    if (this.members.length<1||Number(this.numOfTeams)<1||Number(this.numOfTeams)>this.members.length)
+    {
+      this.errorMessage="Invalid Input";
+      return ;
+    }
+    this.errorMessage="";
+    this.teams=[];
+    let totalMembers=[...this.members];
+    const teamsize=Math.ceil(totalMembers.length/Number(this.numOfTeams));
+    outerLoop:
+    for (let i=0;i<Number(this.numOfTeams);i++)
+    {
+      for (let j=0;j<teamsize;j++)
+      {
+          let randomIndex=Math.floor(Math.random()*totalMembers.length);
+          if (!this.teams[i])
+          {
+            this.teams[i]=[];
+          }
+          this.teams[i].push(totalMembers[randomIndex]);
+          totalMembers.splice(randomIndex,1);
+          if (totalMembers.length==0)
+            break outerLoop;
+          
+      }
+    }
+    console.log("Team Size :",teamsize)
+    console.log(this.teams,totalMembers);
+    this.members=[];
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team/team.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +15,7 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  addMemberName="";
+  members:string[]=[];
+}
